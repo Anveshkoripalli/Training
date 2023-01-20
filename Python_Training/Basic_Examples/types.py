6659c3566c37dd7bdac1abbb4cfe0da5aa629795
Anvesh <anvesh.koripalli9@gmail.com>
2023-01-20 15:06:24 -0800

    Example of types

---
+++ Python_Training/Basic_Examples/types.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+print(len("Hello World"))
+
+print(type("hello world"))  # This gives as string
+
+print(type(9))  # This gives as int
+
+print(type(9.5))  # This gives float
+
+print(type(True)) # This gives as boolean
+
+
+
+
+
+
